26aacdd7de434d6bf6f755fea4059ea49ecd9e34
Artem Safronov <a.safronov@haulmont.com>
2026-05-12 15:14:01 +0400

    init

---
+++ src/pages/NotFoundPage/index.tsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.scss';
+import heroImage from '/images/about-hero-screen.png';
+
+export default function NotFoundPage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.code}>
+        <span className={styles.digit}>4</span>
+        <img src={heroImage} alt="" className={styles.image} />
+        <span className={styles.digit}>4</span>
+      </div>
+    </div>
+  );
+}
